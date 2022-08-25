9f4999b66bf75e98dcbd3ad16a0a531a2ba032fd
Dan Yee <dyee3435@gmail.com>
2022-08-25 10:10:42 -0400

    added comments and documentation

--- Assignment04/assignment4.py
+++ Assignment04/assignment4.py
@@ -2,11 +2,17 @@
 availableItems = ["MILK", "EGGS", "BREAD", "WATER", "COFFEE", "POTATOES", "LETTUCE", "PASTA", "RICE", "BEANS"]
 cart = list()
 
+"""
+Function to display items either available on the shelf (availableItems) or in the users cart (cart)
+"""
 def displayItems(items):
     for item in items:
         print(item.title(), " ", end = "")
     print("\n")
 
+"""
+Function to print the available actions for the user to select from
+"""
 def printActions():
     print("What would you like to do?")
     print("\t- Add Item to Shopping Cart (Enter \"Add\")")
@@ -28,7 +34,7 @@ while(isAddingItems):
         action = input("Select an Action: ")
         action = action.lower()
     
-    if(action == "add"):
+    if(action == "add"):                                                                            # if user wants to add item to cart
         print("\nThese items are currently available:")
         displayItems(availableItems)
         item = input("What item would you like to add to cart?: ")
@@ -40,7 +46,7 @@ while(isAddingItems):
             displayItems(cart)
         else:
             print("Sorry, that item is unavailable.")
-    elif(action == "remove"):
+    elif(action == "remove"):                                                                       # if user wants to remove item from cart
         print("\nThese items are currently in your cart:")
         displayItems(cart)
         item = input("What item would you like to remove from your cart?: ")
@@ -52,11 +58,11 @@ while(isAddingItems):
             displayItems(cart)
         else:
             print("Sorry that item is not currently in your cart.")
-    elif(action == "quit"):
+    elif(action == "quit"):                                                                         # if user wants to empty their cart and start over
         availableItems.extend(cart)
         cart.clear()
         print("\nCart emptied.")
-    elif(action == "leave"):
+    elif(action == "leave"):                                                                        # if user is done shopping and wishes to leave
         print("\nThank you for visiting PyMarket. These are the items you have selected to buy:")
         displayItems(cart)
         isAddingItems = False
